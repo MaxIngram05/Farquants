6ebc7f08c2a1b4fb75f4d6aa930a31da574b9b57
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2026-03-21 16:16:00 -0400

    Added Basic Globals

---
+++ shrekathon/scripts/Globals/dialogue_global.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var icons = {
+	"shrek": 0,
+	"donkey": 1,
+	"pussBoots": 2,
+	"rumpel": 3,
+	"mirror": 4,
+	"fiona": 5
+}
+
+var mirror_dialogue = 0
+var donkey_dialogue = 0
+var puss_dialogue = 0
+var rumpel_dialogue = 0
+var fiona_dialogue = 0
+
+var donkey_choice_text = 0
+var puss_choice_text = 0
+var rumpel_choice_text: Array[String] = ["Sign The Contract", "Refuse to Sign"]
+var mirror_choice_text: Array[String] = ["Fine", "No"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
