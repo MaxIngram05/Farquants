c5279e8384c9701af6dd95182cff4126939a006d
NicolassBoyer <nicolasbernardboyer@gmail.com>
2026-03-21 18:43:07 -0400

    level

--- shrekathon/scripts/jump_king_camera.gd
+++ shrekathon/scripts/jump_king_camera.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 # Viewport is 1280x960 with zoom 4x4, so world screen size = 320x240
 # screen_height set to 100 for testing (first snap triggers at y = -100)
-var screen_width = 427
+var screen_width = 430
 var screen_height = 320
 
 func _process(_delta):
