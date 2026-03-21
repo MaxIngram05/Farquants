5fb2f5987365d1709cde999d428f2366df1fe9c3
Simon Ménard <poisonedcheeto@gmail.com>
2026-03-21 14:33:27 -0400

    Working Mosquito Enemy

---
+++ shrekathon/scenes/Mosquito.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+
+public partial class Mosquito : CharacterBody2D
+{
+	public int speed = 100;
+	public Vector2 velocity;
+	public CharacterBody2D shrek;
+	public bool right = true;
+	public Vector2 shrekDirection;
+	Vector2 moveDirection = new Vector2(1, 1); // Moves Down-Right
+	public bool activated = false;
+	private RandomNumberGenerator rng = new RandomNumberGenerator();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		shrek = GetTree().CurrentScene.GetNode<CharacterBody2D>("Shrek");
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+	
+	public override void _PhysicsProcess(double delta){
+		if(activated){
+			Move();
+		}
+	}
+
+	private void Move()
+	{
+		// 1. Assign to the BUILT-IN Velocity property
+		Velocity = shrekDirection * speed;
+
+		// 2. Tell Godot: "Take my current Velocity and move me now!"
+		MoveAndSlide();
+	}
+	
+	private void OnBodyEntered(Node body)
+	{                 
+		// Push player then die
+		GD.Print("Area entered by: " + body.Name);
+		if (body.Name == "Shrek") 
+		{
+			GD.Print("Success! Mosquito hit Shrek.");
+			// Push shrek
+			shrek.GlobalPosition += new Vector2(moveDirection.X,0) * 40;
+			Kill();
+		}
+		
+		//ToggleDirection();
+	}
+
+	public void Kill(){
+		QueueFree();  
+	}
+	
+	public void Activate(Node body){ 
+		if(!activated){
+			shrekDirection = (this.shrek.GlobalPosition - GlobalPosition).Normalized();
+			activated = true;
+		}
+	}
+	
+	/* Antequated logic
+	
+	private void ToggleDirection(){
+		if(right){
+			GD.Print("We going left now.");
+			moveDirection = new Vector2(1, 1);
+		}else{
+			GD.Print("We going right now.");
+			moveDirection = new Vector2(-1, 1);
+		}
+		
+		right = !right;
+	} */
+}
