db4d7deddacf1363dd92761bb752c53953ee7cfc
Simon Ménard <poisonedcheeto@gmail.com>
2026-03-22 17:11:08 -0400

    Nicklevel default

--- shrekathon/scripts/main_menu.gd
+++ shrekathon/scripts/main_menu.gd
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/DialogueMaking/dialogueTestLevel.tscn")
+	get_tree().change_scene_to_file("res://scenes/nicklevel.tscn")
 
 
 func _on_credits_button_pressed() -> void:
